211f2f9e792f9583ae8bf9a907cf1910a10c1479
Darryen Sands <darryensands@gmail.com>
2022-01-28 18:40:33 -0500

    Added our current dataset, a file to create our training data, a file to generate our model, and a file to create the dataset itself.

---
+++ CNNTraining.py
@@ -0,0 +1,71 @@
+
+import matplotlib.pyplot as plt
+import imagesize
+import numpy as np
+import os
+import cv2
+import pickle 
+'''
+# Get the Image Resolutions
+root = "/home/darryen/Documents/Dataset/"
+imgs = [img.name for img in Path(root).iterdir() if img.suffix == ".png"]
+img_meta = {}
+for f in imgs: img_meta[str(f)] = imagesize.get(root+f)
+
+# Convert it to Dataframe and compute aspect ratio
+img_meta_df = pd.DataFrame.from_dict([img_meta]).T.reset_index().set_axis(['FileName', 'Size'], axis='columns', inplace=False)
+img_meta_df[["Width", "Height"]] = pd.DataFrame(img_meta_df["Size"].tolist(), index=img_meta_df.index)
+img_meta_df["Aspect Ratio"] = round(img_meta_df["Width"] / img_meta_df["Height"], 2)
+
+print(f'Total Nr of Images in the dataset: {len(img_meta_df)}')
+print(img_meta_df.head())
+'''
+
+
+classNames = ["Signal", "Noise"] # These are the names of the folders 
+directory = "/home/darryen/Documents/thesis-project-2021/Dataset/"
+trainingData = []
+imgSize = 50
+
+def CreateTrainingData():
+    for category in classNames:
+        path = os.path.join(directory, category)
+        classNumber = classNames.index(category)
+        for img in os.listdir(path):
+            imgArray = cv2.imread(os.path.join(path, img), cv2.IMREAD_UNCHANGED)
+            print('Original Dimensions : ',imgArray.shape)
+            newArray = cv2.resize(imgArray, (imgSize, imgSize))
+            print('Resized Dimensions : ',newArray.shape)
+            trainingData.append([newArray, classNumber])
+           
+
+CreateTrainingData()
+
+
+
+
+#np.random.shuffle(trainingData)
+
+X = [] # The features (e.g the image data)
+y = [] # The labels (e.g. Whether the image has a signal or not)
+
+for features, label in trainingData:
+    X.append(features)
+    y.append(label)
+
+X = np.array(X)
+y = np.array(y)
+
+print(X.shape[0])
+print(y.shape[0])
+
+pickle_out = open("X.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
---
+++ ModelGenerator.py
@@ -0,0 +1,91 @@
+import tensorflow as tf 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+from keras.models import model_from_json
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+X = pickle.load(open("X.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
+
+X = X/255.0 # Normalizes our data from 0 to 1
+
+
+print(X.shape)
+print(X.shape[1])
+print(X.shape[1:])
+print(X[0])
+print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 0.1)
+print(f"X_train: {X_train}")
+print(f"X_test: {X_test}")
+print(f"y_train: {y_train}")
+print(f"y_test: {y_test}")
+
+print(f"Number of training samples: {len(X_train)}, {len(y_train)} ")
+print(f"Number of testing samples: {len(X_test)}, {len(y_test)} ")
+
+
+
+
+epoch = 3
+batchSize = 32
+
+# building the actual model 
+model = Sequential()
+
+# 3 Convolutional layers 
+model.add(Conv2D(32, (3,3), input_shape = X_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size= (2,2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation("relu"))
+
+model.add(Dense(128))
+model.add(Activation("relu"))
+
+model.add(Dense(1))
+model.add(Activation("softmax"))
+
+model.summary()
+
+model.compile(loss="binary_crossentropy",
+				optimizer="adam",
+				metrics=["accuracy"])
+
+history = model.fit(X_train, y_train, batch_size=batchSize, epochs=epoch, validation_data = (X_test, y_test))
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file :
+	json_file.write(model_json)
+
+model.save_weights("model.h5")
+print("Saved model to disk")
+
+model.save('CNN.model')
+
+# Printing a graph showing the accuracy changes during the training phase
+print(history.history.keys())
+plt.figure(1)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
